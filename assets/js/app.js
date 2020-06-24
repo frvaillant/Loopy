1b8772b5f37420846563890b5cf2aa57717359ae
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-06-24 16:33:51 +0200

    Initialisation project

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// Font Awesome
+import '@fortawesome/fontawesome-free/js/all';
+
+// SCSS Compiler
+import '../scss/app.scss';
+
+// jQuery
+const $ = require('jquery');
+
+// Bootstrap
+require('bootstrap');
