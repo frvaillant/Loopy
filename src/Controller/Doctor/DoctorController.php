4e22fb268d98c527fb5530c146db7fc65d6c5c18
Amelie <a.grn33@gmail.com>
2020-06-24 20:57:00 +0200

    begin

--- src/Controller/Doctor/DoctorController.php
+++ src/Controller/Doctor/DoctorController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller\Doctor;
 
+use App\Repository\PatientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DoctorController extends AbstractController
 {
     /**
      * @Route("/doctor", name="doctor")
+     * @param PatientRepository $patientRepository
+     * @return Response
      */
-    public function index()
+    public function index(PatientRepository $patientRepository)
     {
         return $this->render('doctor/index.html.twig', [
-            'controller_name' => 'DoctorController',
+            'patients'=> $patientRepository->findAll()
         ]);
     }
 }
