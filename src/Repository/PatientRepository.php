eec3fb71a642faf233db75dcead28d3cb2e08cc2
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-06-25 18:47:57 +0200

    fucking category

--- src/Repository/PatientRepository.php
+++ src/Repository/PatientRepository.php
@@ -40,7 +40,7 @@ class PatientRepository extends ServiceEntityRepository
 
         $patient = $this->find($patientId);
         $data = new Data();
-        $category = $categoryRepository->find(1);
+        $category = $categoryRepository->find([]);
         $data->setPatient($patient);
         $data->setValue($glycemy);
         $data->setDataCategory($category);
