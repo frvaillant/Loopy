451803758fc70c422911bbe647a2326dd8f839c3
francoisvaillant <frvaillant@gmail.com>
2020-06-25 16:47:31 +0200

    add patient id in home route

--- src/Controller/Patient/PatientController.php
+++ src/Controller/Patient/PatientController.php
@@ -20,12 +20,12 @@ use \DateTime;
 class PatientController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/{id}", name="home")
      * @return Response
      */
-    public function index(PatientRepository $patientRepository)
+    public function index($id, PatientRepository $patientRepository)
     {
-        $patient = $patientRepository->findOneById(22);
+        $patient = $patientRepository->findOneById($id);
         return $this->render('patient/index.html.twig', [
                 'patient' => $patient
         ]);
@@ -65,7 +65,7 @@ class PatientController extends AbstractController
     }
 
     /**
-     * @Route("/parent/{id}", name="send_value")
+     * @Route("/parent/{id}", name="parent")
      * @param Patient $patient
      * @return Response
      */
